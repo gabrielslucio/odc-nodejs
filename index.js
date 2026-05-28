fa8cb34709abd967df6b1c90b8c331cceb956698
Gabriel <gabrielsilvalucio@gmail.com>
2026-05-28 12:17:07 +0100

    Server setup and cleanup

--- index.js
+++ index.js
@@ -1,20 +1,5 @@
-const express = require('express');
-const cors = require('cors');
+const app = require('./app');
 
-const app = express();
-
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-// Routes
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Smart Porfolio API is running',
-  });
-});
-
-// Start the server
 const PORT = 3000;
 
 app.listen(PORT, () => {
